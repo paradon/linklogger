50d554023f97af319cd25caf82d19fc176e77f21
Thomas Matysik <thomas@matysik.co.nz>
2022-12-26 13:56:00 +1300

    Boilerplate Express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hai');
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hai');
+});
+
+app.listen(port, () => {
+  console.log(`[server] Listening on http://localhost:${port}`);
+});
