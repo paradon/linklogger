9cd46ac31973eaf11adfbd054e5661d672dc3389
Thomas Matysik <thomas@matysik.co.nz>
2022-12-31 18:35:48 +1300

    Add API call

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from 'express';
+import { appendFile } from 'node:fs/promises';
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
+const file = process.env.INBOXFILE || 'inbox.org';
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hai');
+});
+
+app.post('/api/capture', async (req: Request, res: Response) => {
+  await appendFile(
+    file,
+    `* [[${req.body.url}][${req.body.title}]]\n${req.body.notes}\n`,
+  );
+  res.json({ status: 'success' });
 });
 
 app.listen(port, () => {
