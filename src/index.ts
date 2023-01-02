d798b21f2220290015faad40cd5690dfb995a1cd
Thomas Matysik <thomas@matysik.co.nz>
2023-01-03 12:13:57 +1300

    Refactor capture call

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import { appendFile } from 'node:fs/promises';
 
 const app: Express = express();
@@ -11,13 +11,53 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hai');
 });
 
-app.post('/api/capture', async (req: Request, res: Response) => {
-  await appendFile(
-    file,
-    `* [[${req.body.url}][${req.body.title}]]\n${req.body.notes}\n`,
-  );
-  res.json({ status: 'success' });
-});
+function headingTemplate({ url, title }: { url?: string; title?: string }) {
+  if (!url) {
+    return title;
+  }
+  if (!title) {
+    return `[[${url}]] :website:`;
+  }
+  return `[[${url}][${title}]] :website:`;
+}
+
+function bodyTemplate(text?: string) {
+  if (!text) return '';
+  return `${text}\n`;
+}
+
+function nodeTemplate({
+  url,
+  title,
+  notes,
+}: {
+  url?: string;
+  title?: string;
+  notes?: string;
+}) {
+  const heading = headingTemplate({ url, title });
+  const body = bodyTemplate(notes);
+  return `* ${heading}\n${body}`;
+}
+
+app.post(
+  '/api/capture',
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (!(req.body.url || req.body.title)) {
+      return next(new Error('Neither title nor url are specified.'));
+    }
+    await appendFile(
+      file,
+      nodeTemplate({
+        url: req.body.url,
+        title: req.body.title,
+        notes: req.body.notes,
+      }),
+    );
+    res.json({ status: 'success' });
+  },
+);
+
 
 app.listen(port, () => {
   console.log(`[server] Listening on http://localhost:${port}`);
