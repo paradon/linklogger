d2f14115e3283b3d146a6cca0df077e24ed898d7
Thomas Matysik <thomas@matysik.co.nz>
2023-07-01 14:15:33 +1200

    Support multiple org-files

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,55 @@ import { appendFile } from 'node:fs/promises';
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
-const file = process.env.INBOXFILE || 'inbox.org';
+
+const argValues = process.argv.slice(2);
+
+if (argValues.length < 1) {
+  console.log(
+    `Usage: ${process.argv[0]} ${process.argv[1]} inbox=/home/user/inbox.org [ example=/home/user/another.org ...]`,
+  );
+  process.exit();
+}
+
+interface OrgFile {
+  name: string;
+  path: string;
+}
+
+type OrgFileMap = Record<string, OrgFile>;
+
+function parseFileArg(arg: string): OrgFile {
+  const idx = arg.indexOf('=');
+  if (idx < 0) {
+    return <OrgFile>{ name: arg, path: arg };
+  }
+  const name = arg.slice(0, idx);
+  const file = arg.slice(idx + 1);
+  if (name.length < 1) {
+    return <OrgFile>{ name: file, path: file };
+  }
+  if (file.length < 1) {
+    throw new Error('filename missing');
+  }
+  return <OrgFile>{ name: name, path: file };
+}
+
+const orgFiles: OrgFile[] = argValues.map((x) => parseFileArg(x));
+const orgFilesMap: OrgFileMap = orgFiles.reduce(
+  (a, x) => ({ ...a, ...(<OrgFileMap>{ [x.name]: x }) }),
+  <OrgFileMap>{},
+);
+
+function getFileByName(name: string) {
+  if (!name) {
+    return orgFiles[0];
+  }
+  const file = orgFilesMap[name];
+  if (!file) {
+    return orgFiles[0];
+  }
+  return file;
+}
 
 app.use(express.json());
 
@@ -40,6 +88,10 @@ function nodeTemplate({
   return `* ${heading}\n${body}`;
 }
 
+app.get('/api/targets', (req: Request, res: Response) => {
+  return res.json(orgFiles.map((x) => x.name));
+});
+
 app.post(
   '/api/capture',
   async (req: Request, res: Response, next: NextFunction) => {
@@ -47,7 +99,7 @@ app.post(
       return next(new Error('Neither title nor url are specified.'));
     }
     await appendFile(
-      file,
+      getFileByName(req.body.target).path,
       nodeTemplate({
         url: req.body.url,
         title: req.body.title,
